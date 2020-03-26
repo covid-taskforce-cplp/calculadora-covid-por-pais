f14fe9f6530b7b4f19d7a3c3c154f3eeb5426a1e
Emerson Rocha Luiz <emerson@alligo.com.br>
2020-03-26 16:57:30 -0300

    v0.1.0: MVP mais basico possivel; criado covidLetalidadeSimplificada

--- js/calculadora-logica.js
+++ js/calculadora-logica.js
@@ -1,1 +1,24 @@
-console.log('calculadora-logica.js: escreva funcoes simples e reusaveis aqui')
+/**
+* Arquivo com funções escritas em JavaScript com cálculos principais.
+* Favor evitar refatorar as funcões de calculadora-logica.js de modo que fique
+* mais difícil para não-especialistas em JavaScript (ex.: pesquisadores
+*  academicos)
+*
+* @license Public Domain
+*/
+
+/**
+ * Simulação mais simplificada possível de, para uma data população total e
+ * a chance da respectiva população ser infectada (por exemplo, simulando uma
+ * hipotética data final) e a chance de letalidade para respectiva população
+ * retorna quantos óbitos ocorreriam.
+ *
+ * @param {Integer} populacaoTotal               - Total de população na região
+ * @param {Float}   populacaoInfectadaPercentual - Percentual simulado que seria infectado
+ * @param {Float}   letalidadeInfeccao           - Chance infectados virem a óbito
+ */
+function covidLetalidadeSimplificada(populacaoTotal, populacaoInfectadaPercentual, letalidadeInfeccao) {
+  var populacaoInfectada = populacaoTotal * populacaoInfectadaPercentual / 100;
+  var populacaoObito = populacaoInfectada * letalidadeInfeccao / 100;
+  return populacaoObito;
+}
--- js/calculadora-ui.js
+++ js/calculadora-ui.js
@@ -1,1 +1,18 @@
 console.log('calculadora-ui.js pode ser usada para, ao consumir calculadora-logica.js, manipilar interface de usuario')
+
+var populacaoTotal = jQuery('#populacao-total').val().replace(/[^0-9]/g, '');
+var populacaoInfectadaPercentual = jQuery('#populacao-infectada-percentual').val();
+var letalidadeInfeccao = jQuery('#epidemia-letalidade-media').val();
+console.log(populacaoTotal, populacaoInfectadaPercentual, letalidadeInfeccao);
+
+// https://blog.abelotech.com/posts/number-currency-formatting-javascript/
+function formatNumber(num) {
+  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+var populacaoObito = covidLetalidadeSimplificada(populacaoTotal, populacaoInfectadaPercentual, letalidadeInfeccao);
+// console.log('populacaoObito', populacaoObito);
+// console.log('populacaoObito', formatNumber(populacaoObito));
+jQuery('#resultado-total-inicial').html(formatNumber(populacaoTotal));
+jQuery('#resultado-vivos').html(formatNumber(populacaoTotal - populacaoObito));
+jQuery('#resultado-obitos').html(formatNumber(populacaoObito));
