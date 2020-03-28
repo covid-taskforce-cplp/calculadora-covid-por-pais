8fc860c47aceafd2e6a06a7ac69a89332e38d98f
Emerson Rocha Luiz <emerson@alligo.com.br>
2020-03-27 23:43:56 -0300

    populacao-pre-definida (#3): iniciado rascunho de populações pre-definidas

---
+++ js/calculadora-dados.js
@@ -0,0 +1,20 @@
+/**
+* TODO: refatorar esse arquivo, calculadora-dados.js, de forma mais dinamica.
+*       neste momento ele está criando variaveis globais
+*
+* @license PUBLIC DOMAIN
+*/
+
+console.log('calculadora-dados.js inicializado');
+
+dados_populacao_customizado = {
+  _metadata: "Customizado"
+}
+dados_populacao_brasil = {
+  populacaoTotal: 209300000,
+  _metadata: "209,3 milhões (2017), As fontes incluem: Banco Mundial, Departamento do Censo dos Estados Unidos"
+}
+dados_populacao_brasil_rs = {
+  populacaoTotal: 11329605,
+  _metadata: "11 329 605 hab. (2018) estimada. https://pt.wikipedia.org/wiki/Rio_Grande_do_Sul"
+}
