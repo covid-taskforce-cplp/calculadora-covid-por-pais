696bff566b90431f4d32b0b37e2e196d371edffa
GustavoDuregger <gustavo.duregger@gmail.com>
2020-03-29 01:35:15 -0300

    Insercao de todos os estados brasileiros

--- js/calculadora-dados.js
+++ js/calculadora-dados.js
@@ -19,6 +19,139 @@ dados_populacao_brasil_rs = {
   populacaoTotal: 11329605,
   _metadata: "11 329 605 hab. (2018) estimada. https://pt.wikipedia.org/wiki/Rio_Grande_do_Sul"
 }
+
+dados_populacao_brasil_am = {
+  populacaoTotal: 4144597,
+  _metadata: "4 144 597 hab. (2019) estimada. https://pt.wikipedia.org/wiki/Amazonas"
+}
+
+dados_populacao_brasil_ce = {
+  populacaoTotal: 9075649,
+  _metadata: "9 075 649 hab. (2018) estimada. https://pt.wikipedia.org/wiki/Cear%C3%A1"
+}
+
+dados_populacao_brasil_sc = {
+  populacaoTotal: 6910553,
+  _metadata: "6 910 553 hab. (2016) estimada. https://pt.wikipedia.org/wiki/Santa_Catarina"
+}
+
+dados_populacao_brasil_mg = {
+  populacaoTotal: 21119536,
+  _metadata: "21 119 536 hab. (2017) estimada. https://pt.wikipedia.org/wiki/Minas_Gerais"
+}
+
+dados_populacao_brasil_pr = {
+  populacaoTotal: 11348937,
+  _metadata: "11 348 937 hab. (2018) estimada. https://pt.wikipedia.org/wiki/Paran%C3%A1"
+}
+
+dados_populacao_brasil_ba = {
+  populacaoTotal: 14873064,
+  _metadata: "14 873 064 hab. (2019) estimada. https://pt.wikipedia.org/wiki/Bahia"
+}
+
+dados_populacao_brasil_sp = {
+  populacaoTotal: 45919049,
+  _metadata: "45 919 049 hab. (2019) estimada. https://pt.wikipedia.org/wiki/S%C3%A3o_Paulo_(estado)"
+}
+
+dados_populacao_brasil_go = {
+  populacaoTotal: 6921161,
+  _metadata: "6 921 161 hab. (2018) estimada. https://pt.wikipedia.org/wiki/Goi%C3%A1s"
+}
+
+dados_populacao_brasil_pe = {
+  populacaoTotal: 9557071,
+  _metadata: "9 557 071  hab. (2019) estimada. https://pt.wikipedia.org/wiki/Pernambuco"
+}
+
+dados_populacao_brasil_pa = {
+  populacaoTotal: 8578051,
+  _metadata: "8 578 051   hab. (2018) estimada. https://pt.wikipedia.org/wiki/Par%C3%A1"
+}
+
+dados_populacao_brasil_pb = {
+  populacaoTotal: 3996496,
+  _metadata: "3 996 496  hab. (2018) estimada. https://pt.wikipedia.org/wiki/Para%C3%ADba"
+}
+
+dados_populacao_brasil_es = {
+  populacaoTotal: 4016356,
+  _metadata: "4 016 356  hab. (2018) estimada. https://pt.wikipedia.org/wiki/Esp%C3%ADrito_Santo_(estado)"
+}
+
+dados_populacao_brasil_pi = {
+  populacaoTotal: 3264531,
+  _metadata: "3 264 531 hab. (2018) estimada. https://pt.wikipedia.org/wiki/Piau%C3%AD"
+}
+
+dados_populacao_brasil_ma = {
+  populacaoTotal: 7035055,
+  _metadata: "7 035 055  hab. (2018) estimada. https://pt.wikipedia.org/wiki/Maranh%C3%A3o"
+}
+
+dados_populacao_brasil_mt = {
+  populacaoTotal: 3441998,
+  _metadata: "3 441 998 hab. (2018) estimada. https://pt.wikipedia.org/wiki/Mato_Grosso"
+}
+
+dados_populacao_brasil_ms = {
+  populacaoTotal: 2748023,
+  _metadata: "2 748 023 hab. (2018) estimada. https://pt.wikipedia.org/wiki/Mato_Grosso_do_Sul"
+}
+
+dados_populacao_brasil_al = {
+  populacaoTotal: 3337357,
+  _metadata: "3 337 357  hab. (2019) estimada. https://pt.wikipedia.org/wiki/Alagoas"
+}
+
+dados_populacao_brasil_to = {
+  populacaoTotal: 1572866,
+  _metadata: "1 572 866 hab. (2019) estimada. https://pt.wikipedia.org/wiki/Tocantins"
+}
+
+dados_populacao_brasil_rj = {
+  populacaoTotal: 16718956,
+  _metadata: "16 718 956  hab. (2017) estimada. https://pt.wikipedia.org/wiki/Rio_de_Janeiro_(estado)"
+}
+
+dados_populacao_brasil_se = {
+  populacaoTotal: 2278308,
+  _metadata: "2 278 308  hab. (2018) estimada. https://pt.wikipedia.org/wiki/Sergipe"
+}
+
+dados_populacao_brasil_rn = {
+  populacaoTotal: 3479010,
+  _metadata: "3 479 010  hab. (2018) estimada. https://pt.wikipedia.org/wiki/Rio_Grande_do_Norte"
+}
+
+dados_populacao_brasil_ac = {
+  populacaoTotal: 869265,
+  _metadata: "869 265   hab. (2018) estimada. https://pt.wikipedia.org/wiki/Acre"
+}
+
+dados_populacao_brasil_ro = {
+  populacaoTotal: 1787279,
+  _metadata: "1 787 279  hab. (2016) estimada. https://pt.wikipedia.org/wiki/Rond%C3%B4nia"
+}
+
+dados_populacao_brasil_rr = {
+  populacaoTotal: 605761,
+  _metadata: "605 761  hab. (2016) estimada. https://pt.wikipedia.org/wiki/Roraima"
+}
+
+dados_populacao_brasil_ap = {
+  populacaoTotal: 859494,
+  _metadata: "859 494  hab. (2018) estimada. https://pt.wikipedia.org/wiki/Amap%C3%A1"
+}
+
+dados_populacao_brasil_df = {
+  populacaoTotal: 2974703,
+  _metadata: "2 974 703 hab. (2018) estimada. https://pt.wikipedia.org/wiki/Distrito_Federal_(Brasil)"
+}
+
+
+
 dados_populacao_debug = {
   populacaoTotal: 106040,
   populacaoTotalDe0Ate10: 10,
