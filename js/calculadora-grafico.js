0652a7c88630602681cb48f0829b91ca3c4e1649
GustavoDuregger <gustavo.duregger@gmail.com>
2020-03-29 10:31:54 -0300

    melhorias no grafico

--- js/calculadora-grafico.js
+++ js/calculadora-grafico.js
@@ -3,28 +3,35 @@ var ctx = document.getElementsByClassName("radar");
 var chartGraph= new Chart(ctx, {
     type:'line',
     data:{
-        labels:["0","10","30","40","60","70","80"],
+        labels:["0 - 9","10 - 19","20 - 29","30 - 39","40 - 49","50 - 59","60 - 69", "70 - 79", "+80"],
         datasets:[{
-            label:"infectados",
-            data:[10,20,30,40,50,60,70],
+            label:"Óbitos",
+            data:[0,0.2,0.2,0.2,0.4,1.3,3.6,8.0,14.8],
             borderWidth:6,
             borderColor:'rgba(0, 123, 255,0.85)',
             backgroundColor: 'transparent',
         },
         {
-            label:"óbitos",
-            data:[1,2,8,10,20,20,30,40],
+            label:"Complicações cardiovasculares",
+            data:[0,0.221,0.221,0.221,0.442,1.4365,3.978,8.84,16.354],
             borderWidth:6,
-            borderColor:'rgba(240, 52, 52, 0.85)',
+            borderColor:'rgba(240, 52, 52, 0.25)',
             backgroundColor: 'transparent',
-        } 
+        },
+        {
+            label:"Complicações por diabetes/Hipertensão/Cancer",
+            data:[0,0.212,0.212,0.212,0.424,1.378,3.816,8.48,15.688],
+            borderWidth:6,
+            borderColor:'rgba(255,165,0,0.25)',
+            backgroundColor: 'transparent',
+        }  
     ]
     },
     options:{
         title: {
         display:true,
         fontSize:20,
-        text:"Relação Infectados/óbitos",
+        text:"Relação óbitos por faixa etária",
     },
     labels:{
         fontStyle: "Bold"
